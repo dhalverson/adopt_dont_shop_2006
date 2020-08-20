f3844c8aab706a5a88f679745e60abaf77553495
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-19 21:26:52 -0600

    Adds create method in shelters controller, user story 4 complete

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -10,4 +10,25 @@ class SheltersController < ApplicationController
 
   def new
   end
+
+  def create
+    @new_shelter = Shelter.new({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+
+    @new_shelter.save
+
+    redirect_to '/shelters'
+  end
+
+# private
+#
+#   def shelter_params
+#     params.permit(:name, :address, :city, :state, :zip)
+#   end
+
 end
