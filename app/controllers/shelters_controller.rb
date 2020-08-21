73b8deaaf17d2958194b80ef737c1a1a3d8ea581
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-20 21:32:04 -0600

    Adds edit view and complete user story 5

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -5,30 +5,31 @@ class SheltersController < ApplicationController
   end
 
   def show
-    @shelter = Shelter.find_by(id:params[:id])
+    @shelter = Shelter.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @new_shelter = Shelter.new({
-      name: params[:name],
-      address: params[:address],
-      city: params[:city],
-      state: params[:state],
-      zip: params[:zip]
-      })
-
-    @new_shelter.save
-
+    Shelter.create(shelter_params)
     redirect_to '/shelters'
   end
 
-# private
-#
-#   def shelter_params
-#     params.permit(:name, :address, :city, :state, :zip)
-#   end
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update(shelter_params)
+    redirect_to "/shelters/#{shelter.id}"
+  end
+
+private
+
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 
 end
