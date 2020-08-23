8b7f72b3ff17f0d8ad53b6228cf12cf04cc8996e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-23 15:58:31 -0600

    Add shelter pets create and pet params

--- spec/features/shelter_pets/creation_spec.rb
+++ spec/features/shelter_pets/creation_spec.rb
@@ -18,26 +18,27 @@
 # and I am redirected to the Shelter Pets Index page where I can see the new pet listed
 require "rails_helper"
 
-RSpec.describe "Shelter Pet Creatopm", type: :feature do
+RSpec.describe "Shelter Pet Create", type: :feature do
 
   it "I am taken to '/shelters/:shelter_id/pets/new' where I see a form to add a new adoptable pet" do
-    shelter_1 = Shelter.create(name: "Dan's Dogs",
+    @shelter_1 = Shelter.create(name: "Dan's Dogs",
                                address: "123 Main Street",
                                city: "Denver",
                                state: "CO",
                                zip: "80120")
-    shelter_2 = Shelter.create(name: "Woof Woof Bark Bark",
+    @shelter_2 = Shelter.create(name: "Woof Woof Bark Bark",
                               address: "123 Paw Avenue",
                               city: "Lakewood",
                               state: "CO",
                               zip: "80123")
 
-    visit "/shelters/#{shelter_1.id}/pets"
+    visit "/shelters/#{@shelter_1.id}/pets"
+    expect(page).to have_link("Create Pet")
     click_link "Create Pet"
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/pets/new")
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets/new")
     fill_in :name, with: "Larry"
     click_on "Create Pet"
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}/pets")
     expect(page).to have_content("Larry")
   end
 end
